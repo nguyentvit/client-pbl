c356f876fb59249f518eb579dcd7bd62ecfc3291
Nguyên Trần Văn <102210175@sv1.dut.udn.vn>
2024-01-01 20:59:53 +0700

    sdf

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -41,7 +41,7 @@ const Chat = () => {
       }}
 
     >
-      {userChats?.length < 1 ? null : (
+      {userChats?.length < 0 ? null : (
         <Stack direction="horizontal" gap={4} className="align-items-start">
           <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
             {isUserChatsLoading && <p>Loading chats...</p>}
